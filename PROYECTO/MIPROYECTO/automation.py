786462ddad7b5feda41a82167b006f88bbcf8cb6
nick00las <nicolasalonso.rojas@unmsm.edu.pe>
2023-02-14 04:51:11 -0500

    Se terminan funciones para trabajar la DATA del usuario

--- PROYECTO/MIPROYECTO/automation.py
+++ PROYECTO/MIPROYECTO/automation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from f_usuario import * 
 from f_dolar import * 
-
+import os
 
 while True:
     message="""
@@ -9,10 +9,13 @@ while True:
         2. Insertar Data Automaticamente
         3. Actualizar Data por ID
         4. Crear gráfica de evolución del precio del dolar
-        5. Trabajar la data de los usuarios
+        5. Crear usuario
+        6. Eliminar usuario
+        7. Actualizar usuario
+        0. Salir
     """
     print(message)
-    opcion = input("Selecciona una opción (1,2,3 o 4): ")
+    opcion = input("Selecciona una opción (0,1,2,3,4,5,6 o 7): ")
 
     if opcion == "1":
         try:
@@ -41,8 +44,34 @@ while True:
             print("se actualizo correctamente")
     elif opcion == "4":
         graf_historico()
-    else:
+    elif opcion == "5":
+        try:
+            crear_usuario()
+        except: 
+            print("Error")
+        else:
+            print("se creo el usuario correctamente")
+    elif opcion == "6":
+        try:
+            usuario=input("Ingrese el 'usuario' que desea eliminar: ")
+            eliminar_usuario(usuario)
+        except: 
+            print("Error")
+        else:
+            print("se elimino el usuario correctamente")
+    elif opcion == "7":
+        try:
+            usuario=input("Ingrese el 'usuario' que desea actualizar: ")
+            email=input("Ingrese la nueva direccion de correo: ")
+            update_usuario(usuario,email)
+        except: 
+            print("Error")
+        else:
+            print("se actualizó el usuario correctamente")
+    elif opcion == "0":
         exit()
+    else:
+        print("Seleccionaste una opción inválida, intente de nuevo")
 
     
 
--- PROYECTO/MIPROYECTO/f_usuario.py
+++ PROYECTO/MIPROYECTO/f_usuario.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def leer_parametro_ur():
     usuario=input("Ingrese usuario: ")
-    password=input("Ingrese contraseña ")
+    password=input("Ingrese contraseña: ")
     email=input("Ingrese email: ")
     fullname=input("Ingrese nombre completo: ")
     score=input("Ingrese puntuación: ")
@@ -18,15 +18,48 @@ def crear_usuario():
     conn.commit()
 
 def eliminar_usuario(usuario):
-    #DELETE FROM table_name WHERE condition;
-    conn=sqlite3.connect('tienda.db')
-    delete=f"DELETE * FROM USUARIOS WHERE USUARIO={usuario}"
-    conn.execute(delete)
-    conn.commit()
-
+    if comprobar_vacio() is False:
+        #DELETE FROM table_name WHERE condition;
+        conn=sqlite3.connect('tienda.db')
+        delete=f"DELETE FROM USUARIOS WHERE USUARIO='{usuario}'"
+        conn.execute(delete)
+        conn.commit()
+    else:
+        pass
 
 def update_usuario(usuario,email):
-    conn=sqlite3.connect('tienda.db')
-    update=f"UPDATE USUARIOS SET USUARIO={usuario} WHERE EMAIL={email}"
-    conn.execute(update)
-    conn.commit()
+    if comprobar_vacio() is False:
+        conn=sqlite3.connect('tienda.db')
+        update=f"UPDATE USUARIOS SET USUARIO='{usuario}' WHERE EMAIL='{email}'"
+        conn.execute(update)
+        conn.commit()
+    else:
+        pass
+
+def contar_filas():
+    # Conectarse a la base de datos
+    conn = sqlite3.connect('tienda.db')
+    cursor = conn.cursor()
+
+    # Nombre de la tabla a verificar
+    nombre_tabla = 'USUARIOS'
+
+    # Consulta SQL para contar el número de filas en la tabla
+    consulta = f"SELECT COUNT(*) FROM {nombre_tabla}"
+    cursor.execute(consulta)
+
+    # Obtener el resultado de la consulta
+    filas = cursor.fetchone()[0]
+
+    # Cerrar la conexión a la base de datos
+    conn.close()
+
+    return filas
+
+def comprobar_vacio():
+        # Comprobar si la tabla está vacía
+    filas=contar_filas()
+    if filas == 0:
+        return True
+    else:
+        pass
