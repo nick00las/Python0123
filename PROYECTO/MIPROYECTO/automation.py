d3980b451c118c03a1657f5cc85351a7ab6c7172
nick00las <nicolasalonso.rojas@unmsm.edu.pe>
2023-02-14 00:42:38 -0500

    Funciones de la parte 3 del problema añadidas
    Se crea el menú y sus respectivas funciones para actualizar datos, insertar datos manualmente e insertar datos automaticamente por medio de consulta por la api

--- PROYECTO/MIPROYECTO/automation.py
+++ PROYECTO/MIPROYECTO/automation.py
@@ -1,23 +1,15 @@
 import pandas as pd
-import os
-import db
 import requests
 import sqlite3
+import matplotlib.pyplot as plt
 
-def insertData():
-    #obtiene la ruta absoluta
-     #print(os.getcwd())
-    path_=os.getcwd()+'\dataTienda.csv'
-    '''#conection a bd
-    conn=db.Conection('tienda.db')
-    cursor=conn.getCursor()'''
-    print(path_)
-    df = pd. read_csv (path_, sep = ";") 
-    ### logica para insertar 
-    for i,fila in df.iterrows():
-        print(fila['ORDER_ID'])
+def insertDataManual(valor):
+    conn=sqlite3.connect('tienda.db')
+    insert=f"INSERT INTO DOLAR(DOLAR_SOLES) VALUES('{valor}')"
+    conn.execute(insert)
+    conn.commit()
 
-def updateDolar():
+def insertDataAuto():
     url = 'https://api.apis.net.pe/v1/tipo-cambio-sunat' #tipo cambio sunat
     response = requests.get(url)
     data = response.json()
@@ -27,6 +19,25 @@ def updateDolar():
     conn=sqlite3.connect('tienda.db')
     insert=f"INSERT INTO DOLAR(DOLAR_SOLES) VALUES('{dolar_compra}')"
     conn.execute(insert)
+    conn.commit()
+    
+    """#obtiene la ruta absoluta
+     #print(os.getcwd())
+    path_=os.getcwd()+'\dataTienda.csv'
+    '''#conection a bd
+    conn=db.Conection('tienda.db')
+    cursor=conn.getCursor()'''
+    print(path_)
+    df = pd. read_csv (path_, sep = ";") 
+    ### logica para insertar 
+    for i,fila in df.iterrows():
+        print(fila['ORDER_ID'])"""
+
+def updateDolar(valor_dolar,valor_id):
+    conn=sqlite3.connect('tienda.db')
+    update=f"UPDATE DOLAR SET DOLAR_SOLES={valor_dolar} WHERE IDKEY={valor_id}"
+    conn.execute(update)
+    conn.commit()
 
 '''
 message="""
@@ -34,23 +45,48 @@ message="""
     2)Actualizar data del dolar
 """
 '''
+def graf_historico():
+    pass
 
 while True:
     message="""
-        1. Opción 1
-        2. Opción 2
+        1. Insertar Data Manualmente
+        2. Insertar Data Automaticamente
+        3. Actualizar Data por ID
+        4. Crear gráfica de evolución del precio del dolar
     """
     print(message)
-    opcion = input("Selecciona una opción (1 o 2): ")
+    opcion = input("Selecciona una opción (1,2,3 o 4): ")
 
     if opcion == "1":
-        insertData()
+        try:
+            valor_dolar=input("Ingrese el valor del dolar en soles: ")
+            insertDataManual(valor_dolar)
+        except: 
+            print("Error")
+        else:
+            print("se inserto correctamente")
     elif opcion == "2":
-        updateDolar()
+        try:
+            insertDataAuto()
+        except: 
+            print("Error")
+        else:
+            print("se inserto correctamente")
+
+    elif opcion == "3":
+        try:
+            valor_dolar=input("Ingrese el valor del dolar en soles: ")
+            valor_id=input("Ingrese la ID que quiere reemplazar: ")
+            updateDolar(valor_dolar,valor_id)
+        except: 
+            print("Error")
+        else:
+            print("se actualizo correctamente")
+    elif opcion == "4":
+        pass
     else:
-        print("Opción no válida. Selecciona de nuevo.")
+        exit()
 
     
-
-
 
